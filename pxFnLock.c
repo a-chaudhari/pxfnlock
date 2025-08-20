62f379da25aa74476c9b67fc3fdcbaad30fc17b5
Amit Chaudhari <a-chaudhari@users.noreply.github.com>
2025-08-20 08:05:01 -0400

    Implement state file handling for fn lock functionality

--- pxFnLock.c
+++ pxFnLock.c
@@ -159,6 +159,7 @@ int find_hid_devices_paths(const char *hid_path, hid_sub_paths_t *devices) {
 }
 
 /**
+ * Find a HID device by its VID:PID and verify its report descriptor matches the one we want
  * @param search_id "VID:PID" to search for, e.g. "0B05:19B6"
  * @param info the structure to fill with the found HID device information
  * @return 0 on success, -1 on failure
@@ -225,7 +226,7 @@ int find_hid_id(const char *search_id, hid_device_info_t *info) {
 }
 
 /**
- *
+ * Toggle the fn lock state by sending a HID feature report
  * @param hidraw_path path to specific hidraw device, e.g. /dev/hidraw1
  * @param fn_lock 0 = fn lock on (f1-12 buttons act as hotkeys), 1 = fn lock off (f1-12 buttons act as normal)
  * @return 0 on success, -1 on failure
@@ -259,9 +260,100 @@ int toggle_fnlock(const char *hidraw_path, int fn_lock)
     return 0;
 }
 
+/**
+ * Reads the state file to determine the current fn lock state
+ * The state file is expected to contain a single integer
+ * @return -1 on failure, otherwise 0 for fn_lock on, 1 for fn-lock off
+ */
+int read_statefile()
+{
+    const char* state_file_path = "/var/lib/pxFnLock/state";
+    // need to create the directory if it doesn't exist
+    struct stat st;
+    if (stat("/var/lib/pxFnLock", &st) != 0)
+    {
+        // Directory does not exist, create it
+        if (mkdir("/var/lib/pxFnLock", 0755) != 0)
+        {
+            perror("Failed to create state directory");
+            return -1;
+        }
+    }
+
+    int fd = open(state_file_path, O_RDWR | O_CREAT, 0644);
+    if (fd < 0)
+    {
+        perror("Failed to open state file");
+        return -1;
+    }
+
+    // read contents
+    int buffer;
+    ssize_t bytes_read = read(fd, &buffer, sizeof(buffer));
+    int state = FN_LOCK_DEFAULT_VALUE;
+    if (bytes_read > 0)
+    {
+        // reading worked. convert the int
+        printf("Read state from file: %x\n", buffer);
+        if (buffer == 0 || buffer == 1)
+        {
+            printf("found state in file: %d\n", buffer);
+            close(fd);
+            return buffer;
+        }
+    }
+
+    printf("Invalid state in file, using default value\n");
+    // save the state to the file
+    lseek(fd, 0, SEEK_SET); // reset file pointer to the beginning
+    if (write(fd, &state, sizeof(state)) < 0)
+    {
+        perror("Failed to write default state to file");
+        close(fd);
+        return -1;
+    }
+    close(fd);
+    return state;
+}
+
+/**
+ * Write the current fn lock state to the state file
+ * @param new_state 0 for fn lock on, 1 for fn lock off
+ * @return 0 on success, -1 on failure
+ */
+int write_state(int new_state)
+{
+    // open state file
+    const char* state_file_path = "/var/lib/pxFnLock/state";
+    int fd = open(state_file_path, O_RDWR);
+    if (fd < 0)
+    {
+        perror("Failed to open state file");
+        return -1;
+    }
+
+    size_t bytesWritten = write(fd, &new_state, sizeof(new_state));
+    if (bytesWritten == 0)
+    {
+        perror("Failed to write default state to file");
+        close(fd);
+        return -1;
+    }
+
+    printf("Write state to file: %d\n", new_state);
+    close(fd);
+    return 0;
+}
+
 int main(int argc, char **argv)
 {
-    int fn_state = FN_LOCK_DEFAULT_VALUE;
+    int fn_state = read_statefile();
+    if (fn_state < 0)
+    {
+        printf("Failed to read state file\n");
+        return -1;
+    }
+
     hid_device_info_t device_info;
     hid_sub_paths_t devices;
     int err, evdev_fd;
@@ -325,6 +417,12 @@ int main(int argc, char **argv)
                     printf("Failed to toggle fn lock\n");
                 } else {
                     printf("Fn lock toggled to %s\n", fn_state ? "off" : "on");
+                }
+
+                err = write_state(fn_state);
+                if (err)
+                {
+                    printf("failed to write state file\n");
                 }
             }
         }
